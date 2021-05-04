327067611c8a76c747914e82f86ba4c2b3704e2c
Laura <laurarojaszdo@gmail.com>
2021-05-04 09:01:08 -0600

    cmake basico

---
+++ src/CascoConvexo.cxx
@@ -0,0 +1,16 @@
+#include <iostream>
+
+/**
+ * HEADER PARA LA FUNCIÓN MAIN (falta documentar bien)
+ */
+int main() {
+
+	// Hello world
+    std::cout << "Hello World" << std::endl;
+
+    return 1;
+
+}
+
+
+
