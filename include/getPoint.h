033ae97d7d9cbbccb3809041abac0d2ea7801b7f
Laura <laurarojaszdo@gmail.com>
2021-05-04 14:16:54 -0600

    50% listo, lee csv y aplica cgal

---
+++ include/getPoint.h
@@ -0,0 +1,10 @@
+#ifndef GETPOINT_H_
+#define GETPOINT_H_
+
+/*
+ * @function getPoint
+ * @brief Esta función extrae los puntos de un string que se pasa por parámero. Estos datos se escriben en el vector puntos. También se revisa la validez de estos puntos.
+ */
+void getPoint(string coordinates, double* puntos, bool DEBUG);
+
+#endif // GETPOINT_H_
